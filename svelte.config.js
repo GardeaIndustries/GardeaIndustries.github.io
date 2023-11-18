7efc827de8c14e9d15425826b50c57ce56283bc6
GardeaIndustries <juan.rr0729@gmail.com>
2023-11-17 16:33:47 -0800

    second commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.argv.includes('dev');
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
+
   },
   preprocess: vitePreprocess()
 };
