86d432e565565490c8adf90ab14e29f2316aefb5
Juan C Gardea <152209120+GardeaIndustries@users.noreply.github.com>
2023-12-08 11:38:03 -0800

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -21,7 +21,7 @@
             </a>
         {/each}
     </div>
-    <a href="https://www.linkedin.com/in/juan-rodriguez-26b39427a/" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <a href="https://www.linkedin.com/in/juan-gardea-26b39427a/" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
         <div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
         </div>
         <h4 class="relative z-9">Get in touch!</h4>
